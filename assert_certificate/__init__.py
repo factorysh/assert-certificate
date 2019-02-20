2d547c4c89c321e032ae4c82ced3a7bbe056c2be
Mathieu Lecarme <mlecarme@bearstech.com>
2019-02-20 21:37:51 +0100

    common name

--- assert_certificate/__init__.py
+++ assert_certificate/__init__.py
@@ -4,3 +4,10 @@ from x5092json import x509parser
 def parse_certificate(data):
     certificate = x509parser.READERS['PEM'](data)
     return x509parser.parse(certificate)
+
+
+def subject_common_name(certificate):
+    for s in certificate['subject']:
+        if s['oid']['name'] == 'commonName':
+            return s['value']
+    return None
