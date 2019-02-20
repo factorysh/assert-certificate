96960505af8c32564af190a1285ccddb70c6f849
Mathieu Lecarme <mlecarme@bearstech.com>
2019-02-20 21:59:49 +0100

    dnsname

--- assert_certificate/__init__.py
+++ assert_certificate/__init__.py
@@ -6,8 +6,19 @@ def parse_certificate(data):
     return x509parser.parse(certificate)
 
 
-def subject_common_name(certificate):
-    for s in certificate['subject']:
-        if s['oid']['name'] == 'commonName':
-            return s['value']
+def get(certificate, key1, key2):
+    for a in certificate[key1]:
+        if a['oid']['name'] == key2:
+            return a['value']
     return None
+
+
+def subject_common_name(certificate):
+    return get(certificate, 'subject', 'commonName')
+
+
+def dnsname(certificate):
+    altnames = get(certificate, 'extensions', 'subjectAltName')
+    if altnames is None:
+        return None
+    return [ n['dnsname_value'] for n in altnames]
