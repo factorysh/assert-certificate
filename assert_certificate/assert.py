91041f3fd30ebb06f50639b80f01dfdd9a9a38a5
Mathieu Lecarme <mlecarme@bearstech.com>
2019-02-20 22:09:02 +0100

    assert www

---
+++ assert_certificate/assert.py
@@ -0,0 +1,17 @@
+from assert_certificate import dnsname
+
+
+def assert_www(certificate):
+    """
+    each DNSname has its www counterpart, or a star counterpart
+    """
+    naked = set()
+    www = set()
+    for name in dnsname(certificate):
+        if name.startswith('www.'):
+            www.add(name[4:])
+        elif name.startswith('*.'):
+            www.add(name[2:])
+        else:
+            naked.add(name)
+    assert www == naked
