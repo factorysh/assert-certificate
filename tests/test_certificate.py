dfd1d860759b20ca22bbac8ce05d6fe9764d248d
Mathieu Lecarme <mlecarme@bearstech.com>
2019-02-23 00:22:11 +0100

    More tests

--- tests/test_certificate.py
+++ tests/test_certificate.py
@@ -1,6 +1,7 @@
 import pytest
 
-from assert_certificate import parse_certificate
+from assert_certificate.pool import (load_pem_all_certificates, trusted_ca,
+                                     verify_chain)
 
 
 @pytest.fixture
@@ -9,5 +10,6 @@ def certificate():
 
 
 def test_certificate(certificate):
-    c = parse_certificate(certificate)
-    print(c)
+    ca = trusted_ca()
+    google = load_pem_all_certificates('wwwgooglecom.crt')
+    assert verify_chain(ca, google)
